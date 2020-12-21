06f033594746c93f43c326db6fdb06d7803bdd9b
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-12-21 22:26:47 +0100

    teaching guide

--- rendering-data/examples/1-simple-hyperlink/expected.js
+++ rendering-data/examples/1-simple-hyperlink/expected.js
@@ -2,6 +2,7 @@
 
 // obfuscated solution for rendering expected elements
 const a0_0x2393 = ['_blank', 'href', 'link', 'target', 'innerHTML'];
+
 (function(_0x261186, _0x2393be) {
   const _0x4ce27c = function(_0xacaaa8) {
     while (--_0xacaaa8) {
@@ -10,11 +11,13 @@ const a0_0x2393 = ['_blank', 'href', 'link', 'target', 'innerHTML'];
   };
   _0x4ce27c(++_0x2393be);
 })(a0_0x2393, 0x104);
+
 const a0_0x4ce2 = function(_0x261186, _0x2393be) {
   _0x261186 = _0x261186 - 0x0;
   let _0x4ce27c = a0_0x2393[_0x261186];
   return _0x4ce27c;
 };
+
 const reference = _0x58cc3b => {
   const _0x48f248 = document['createElement']('a');
   return (
